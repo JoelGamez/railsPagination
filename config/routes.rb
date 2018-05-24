830a0486d729bec29dbaa2640a2106d7dbfb1d4b
joel gamez <joelgamez14six@yahoo.com>
2018-05-24 15:30:20 -0400

    make routes prettier

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'statuses', to: 'statuses#index', page: 1
+  get 'statuses/page/:page', to: 'statuses#index'
+
   get 'statuses/index'
   get 'statuses/show'
   get 'statuses/new'
